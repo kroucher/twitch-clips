e34e455e28f760a74dbe23b89853e8ab826fa9cd
kroucher <kroucher.1019@gmail.com>
2022-09-12 15:39:24 +1000

    add clip embed

--- src/pages/user/[username]/clip/[clip].tsx
+++ src/pages/user/[username]/clip/[clip].tsx
@@ -11,8 +11,8 @@ const ClipPage = () => {
     });
     console.log(data, status);
     return (
-        <div>
-            Enter
+        <div className="w-full bg-slate-900 text-white items-center flex justify-center min-h-screen">
+            <iframe src={data?.[0]?.embed_url} className="w-full max-w-4xl h-full" />
         </div>
     );
 }
