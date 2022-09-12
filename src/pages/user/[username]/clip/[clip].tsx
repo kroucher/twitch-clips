c085fd87748590857ab2d7543a81219e290c5f44
kroucher <kroucher.1019@gmail.com>
2022-09-12 16:40:39 +1000

    fix max width

--- src/pages/user/[username]/clip/[clip].tsx
+++ src/pages/user/[username]/clip/[clip].tsx
@@ -11,18 +11,18 @@ const ClipPage = () => {
         enabled: !!clip,
     });
     return (
-        <div className="w-screen bg-slate-900 text-white items-start flex justify-center min-h-screen pb-32">
+        <div className="max-w-screen bg-slate-900 text-white items-start flex justify-center min-h-screen pb-32">
 
             <button onClick={() => router.back()} className="absolute top-0 left-0 m-4 text-2xl border-2 rounded-full">
                 <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20">
                     <path fillRule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clipRule="evenodd" />
                 </svg>
             </button>
-            <div className="flex flex-col items-start justify-center pt-28 w-full">
+            <div className="flex flex-col items-start justify-center pt-28 w-full px-4">
                 <div className="w-3/4 text-left py-8 px-4">
                     <span className="text-2xl sm:text-5xl whitespace-pre-wrap">{data?.[0]?.title}</span>
                 </div>
-                <iframe src={`${data?.[0]?.embed_url}&parent=twitch-clips.beanlea.com`} height="720"
+                <iframe src={`${data?.[0]?.embed_url}&parent=${process.env.NODE_ENV === "production" ? `twitch-clips.beanlea.com` : `localhost:3000`}`} height="720"
                     width="1280"
                     allowFullScreen className="w-full mx-auto px-1" />
                 <div className="flex items-center justify-between w-full px-3">
